9178bf5e903f0ea806223e34589b1a5c04f58d93
SusanLiu0405 <15301609750@163.com>
2026-01-28 18:40:07 -0800

    add lc 215

---
+++ leetcode/0215_kth_largest_element.py
@@ -0,0 +1,11 @@
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        for num in nums:
+            heappush(heap, num)
+            while len(heap) > k:
+                heappop(heap)
+        return heap[0]
+
+sol = Solution()
